9cb8b030482d9594b7cf347d8f19e6eaee7f93c1
Matt Gleeson <matt@gleeson.org>
2010-01-19 15:51:12 -0800

    fix malformed html/css when noisy comments end up in unfortunate places

--- lib/noisy_partials.rb
+++ lib/noisy_partials.rb
@@ -4,7 +4,15 @@ module ActionView
       partial_path = args[0][:partial]
       start_explanation = "\n<!-- START PARTIAL #{partial_path} -->\n"
       end_explanation = "\n<!-- END PARTIAL #{partial_path} -->\n"
-      start_explanation + render_partial_without_explanation(*args).to_s + end_explanation
+      content = render_partial_without_explanation(*args).to_s
+      # ghetto html detector.  try not to put html comments into
+      # non-html content or around the very beginning or end of an
+      # html document.  it can break layout on at least ie6.
+      if content =~ /[<>]/  && content !~ /(<\/html>|DOCTYPE)/i
+        start_explanation + content + end_explanation
+      else
+        content
+      end
     end
 
     alias_method_chain :render_partial,:explanation     
